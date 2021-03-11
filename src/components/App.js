0f760ef445acadda2f7f97d981077df9250c2e70
Nghia <hello.nghia22@gmail.com>
2021-03-11 16:35:15 -0500

    feat: Build static version of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
-import * as R from 'ramda'
-
 export default () => {
-	return <div>App</div>;
+	console.log(object);
+	return (
+		<div className="app">
+			<div className="randomizer">
+				<p>Random</p>
+				<svg>icon</svg>
+			</div>
+			<div className="quote">
+				<div></div>
+				<p>Some kind of random quote text</p>
+			</div>
+			<div className="author">
+				<h2>Author's name</h2>
+				<p>quote genre</p>
+			</div>
+		</div>
+	);
 };
