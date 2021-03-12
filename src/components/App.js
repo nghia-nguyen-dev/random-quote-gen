69ab84b18358ca4bde3526f082aa5b972f637987
Nghia <hello.nghia22@gmail.com>
2021-03-11 23:47:54 -0500

    feat: Add state and renderView ƒ to app

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,46 @@ import QuoteList from "components/QuoteList";
 import { fetchData } from "utils/helpers";
 
 const config = {
-    URL: `https://quote-garden.herokuapp.com/api/v3/quotes/random`,
-}
+	URL: `https://quote-garden.herokuapp.com/api/v3/quotes/random`,
+};
 
 export default () => {
 	const [mainQuote, setMainQuote] = useState([{}]);
+	const [state, setState] = useState(1);
 
 	useEffect(() => {
-		fetchData(
-			setMainQuote,
-			config.URL
-		);
+		fetchData(setMainQuote, config.URL);
 	}, []);
+
+	const renderView = () => {
+		switch (state) {
+			case 1:
+				return (
+					<>
+						<QuoteBlock text={mainQuote[0].quoteText} />
+
+						<div className="author" onClick={() => setState(2)}>
+							<h2>{mainQuote[0].quoteAuthor}</h2>
+							<p>{mainQuote[0].quoteGenre}</p>
+						</div>
+					</>
+				);
+			case 2:
+				return <QuoteList mainQuote={mainQuote[0]} />;
+		}
+	};
 
 	return (
 		<div className="app">
-			<div className="randomizer">
+			<div
+				className="randomizer"
+				onClick={() => fetchData(setMainQuote, config.URL)}
+			>
 				<p>Random</p>
 				<img src={RefreshIcon} />
 			</div>
-			<QuoteBlock text={mainQuote[0].quoteText} />
-			<div className="author">
-				<h2>{mainQuote[0].quoteAuthor}</h2>
-				<p>{mainQuote[0].quoteGenre}</p>
-			</div>
-			<QuoteList mainQuote={mainQuote[0]} />
+
+			{renderView()}
 		</div>
 	);
 };
