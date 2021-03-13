c2dcc000e16c0af7e1162d4f7708416cb570d843
Nghia <hello.nghia22@gmail.com>
2021-03-13 18:34:29 -0500

    feat: Add conditional rendering for text inside of quote block

--- src/components/App.js
+++ src/components/App.js
@@ -38,6 +38,7 @@ export default () => {
 						</div>
 					</>
 				);
+
 			case 2:
 				return <Quotes mainQuote={mainQuote[0]} />;
 		}
--- src/components/QuoteBlock.js
+++ src/components/QuoteBlock.js
@@ -1,9 +1,10 @@
 export default ({ text }) => {
-	console.log(text)
 	return (
 		<li className="quote-block">
 			<div className="quote-block__bar"></div>
-			<h1 className="quote-block__text">"{text}"</h1>
+			<h1 className="quote-block__text">
+				{text !== undefined ? `"${text}"` : null}
+			</h1>
 		</li>
 	);
 };
