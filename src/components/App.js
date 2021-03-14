0f2e3b8a789a5f3f1f080bdab3f55a37c05d5237
Nghia <hello.nghia22@gmail.com>
2021-03-13 19:05:40 -0500

    refactor: Change img tag to svg for arrow icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
-import * as R from "ramda";
 import { useState, useEffect } from "react";
 import refreshIcon from "assets/icons/refresh-icon.svg";
 import QuoteBlock from "components/QuoteBlock";
 import Quotes from "components/Quotes";
 import { fetchData } from "utils/helpers";
-import rightArrow from "assets/icons/right-arrow.svg";
 
 const config = {
 	URL: `https://quote-garden.herokuapp.com/api/v3/quotes/random`,
@@ -34,7 +32,17 @@ export default () => {
 									{mainQuote[0].quoteAuthor}
 								</h2>
 							</div>
-							<img className="info__icon" src={rightArrow} />
+							<svg
+								className="info__arrow"
+								xmlns="http://www.w3.org/2000/svg"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="#333"
+								width="24"
+							>
+								<path d="M0 0h24v24H0z" fill="none" />
+								<path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4z" />
+							</svg>
 						</div>
 					</>
 				);
