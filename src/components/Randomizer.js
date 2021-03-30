d183084727acd6b180e27d2a230053dc1b7e7f85
Nghia <hello.nghia22@gmail.com>
2021-03-30 16:07:14 -0400

    docs: Update readme

--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -2,14 +2,14 @@ import refreshIcon from "assets/icons/refresh-icon.svg";
 import config from "utils/config";
 import { fetchData } from "utils/helpers";
 
-export default ({setMainQuote}) => {
-    return (
-        <div
-        className="randomizer"
-        onClick={() => fetchData(setMainQuote, config.URL)}
-    >
-        <p className="randomizer__text">random</p>
-        <img className="randomizer__icon" src={refreshIcon} />
-    </div>
-    )
-}
+export default ({ setMainQuote }) => {
+	return (
+		<div
+			className="randomizer"
+			onClick={() => fetchData(setMainQuote, config.URL)}
+		>
+			<p className="randomizer__text">random</p>
+			<img className="randomizer__icon" src={refreshIcon} />
+		</div>
+	);
+};
